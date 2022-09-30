77ae21291ac9cf69917e7770bcea304f5b219e84
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2022-09-30 17:03:56 +0300

    viime viikko

---
+++ Player.gd
@@ -0,0 +1,17 @@
+extends KinematicBody
+
+var run_speed : float = 8.0
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
