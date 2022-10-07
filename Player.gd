c22c0b3bad143dd53a9045903c084f944863b33b
Your Name <you@example.com>
2022-10-07 18:17:23 +0300

    collidersit

--- Player.gd
+++ Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody
 
 var run_speed : float = 8.0
+var sidestep_speed : float = 5.0
+var velocity := Vector3()
 
 var time : float = 0.0
 var step_freq : float = 2.0 #kuinka usein
@@ -22,4 +24,14 @@ func _physics_process(delta):
 	body_hinge.translation.y = step_height * (1 + sin(2.0 * PI * step_freq * time))
 	body_hinge.rotation_degrees.z = step_tilt * sin(PI * step_freq * time) 
 	time += delta 
-	move_and_slide(Vector3(0, 0, -run_speed))
+	
+	var sideways: float = 0.0
+	if Input.is_action_pressed("move_right"):
+		sideways += 1.0
+	if Input.is_action_pressed("move_left"):
+		sideways -= 1.0
+	
+	velocity.x = sideways * sidestep_speed
+	velocity.z = -run_speed
+	
+	move_and_slide(velocity)
