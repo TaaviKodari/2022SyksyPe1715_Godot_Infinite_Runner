39dfb468d25b4ed92516275793e1baac5a6b9226
Your Name <you@example.com>
2022-11-11 18:18:11 +0200

    tien palaset kuntoon

--- Main.gd
+++ Main.gd
@@ -8,11 +8,29 @@ var jump_height : float = 2.0
 onready var player = $Player
 onready var camera_pivot = $CameraPivot
 
+var initial_road_count :int = 5
+var road_scenes = [
+	load("res://Road1.tscn"),
+	load("res://Road2.tscn"),
+	load("res://Road3.tscn"),
+]
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	player.setup_jump(jump_length, jump_height, run_speed)
+	
+	for i in range(initial_road_count):
+		var road = make_random_road()
+		road.translation.z = -(i+1) * 50
+		add_child(road)
 
 
 func _physics_process(delta):
 	camera_pivot.translation = player.translation
 	camera_pivot.translation.y = 0
+	
+
+func make_random_road():
+	var road_scene = road_scenes[randi() % road_scenes.size()]
+	var road = road_scene.instance()
+	return road
