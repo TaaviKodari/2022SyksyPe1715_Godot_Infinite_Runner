b06b6b50e98c0406ca21d4db69a951591b5a0258
Your Name <you@example.com>
2022-11-18 18:19:44 +0200

    infinite road tehty

--- Main.gd
+++ Main.gd
@@ -21,16 +21,28 @@ func _ready():
 	
 	for i in range(initial_road_count):
 		var road = make_random_road()
-		road.translation.z = -(i+1) * 50
+		road.translation.z = -(i+1) * RoadBase.LENGTH
 		add_child(road)
 
 
 func _physics_process(delta):
+	if player.translation.z < -RoadBase.LENGTH:
+		player.translation.z += RoadBase.LENGTH
+		
+		for child in get_children():
+			var road = child as RoadBase
+			if road:
+				road.translation.z += RoadBase.LENGTH
+				if road.translation.z > RoadBase.LENGTH:
+					road.queue_free()
+		
+		var new_road := make_random_road()
+		new_road.translation.z = - initial_road_count * RoadBase.LENGTH
+		add_child(new_road) 			
 	camera_pivot.translation = player.translation
 	camera_pivot.translation.y = 0
 	
-
-func make_random_road():
+func make_random_road() -> RoadBase:
 	var road_scene = road_scenes[randi() % road_scenes.size()]
 	var road = road_scene.instance()
 	return road
---
+++ RoadBase.gd
@@ -0,0 +1,4 @@
+class_name RoadBase
+extends Spatial
+const LENGTH: float = 50.0
+
