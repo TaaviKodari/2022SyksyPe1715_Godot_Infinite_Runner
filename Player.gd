e97c71d66cff09d5094704061998a76fbb567017
Your Name <you@example.com>
2022-11-25 18:16:16 +0200

    post-processia

--- Player.gd
+++ Player.gd
@@ -23,14 +23,15 @@ func setup_jump(length : float, height : float, speed : float):
 #	pass
 
 func _physics_process(delta):
-	body_hinge.translation.y = step_height * (1 + sin(2.0 * PI * step_freq * time))
-	body_hinge.rotation_degrees.z = step_tilt * sin(PI * step_freq * time) 
-	time += delta 
+	if is_on_floor():
+		body_hinge.translation.y = step_height * (1 + sin(2.0 * PI * step_freq * time))
+		body_hinge.rotation_degrees.z = step_tilt * sin(PI * step_freq * time) 
+		time += delta 
 	
 	var sideways: float = 0.0
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right") and is_on_floor():
 		sideways += 1.0
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left") and is_on_floor():
 		sideways -= 1.0
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -45,6 +46,6 @@ func _physics_process(delta):
 	for index in range(get_slide_count()):
 		var collision = get_slide_collision(index)
 		var collision_object = collision.collider as CollisionObject
-		if collision_object.collision_layer & 4:
+		if collision_object.collision_layer & 4 and rad2deg(collision.get_angle()) > 60:
 			#print("Ouch!")
 			get_tree().reload_current_scene()
