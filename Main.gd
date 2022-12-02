2d50952a3234c110b19c8d54a6b1f4b09cf48d32
Your Name <you@example.com>
2022-12-02 18:16:52 +0200

    ui aloitettu

--- Main.gd
+++ Main.gd
@@ -17,6 +17,7 @@ var road_scenes = [
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	get_tree().paused = true
 	player.setup_jump(jump_length, jump_height, run_speed)
 	
 	for i in range(initial_road_count):
